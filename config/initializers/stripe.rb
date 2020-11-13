fb6b8c405be6d5386c7b6716dab2845a4d0240b1
Ubuntu <jojorostock@yahoo.com>
2020-11-13 20:54:19 +0000

    fix Stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,4 @@ Rails.configuration.stripe = {
     :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 }
 
-stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
